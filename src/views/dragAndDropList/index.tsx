d0aa86c06a02b5babd099590cd39a6dec043c7f4
Gandhar Rajan Bichkar <bichkargandhar@gmail.com>
2025-09-29 20:18:34 -0700

    Handled onDrag event to remove item from the list and storing it

--- src/views/dragAndDropList/index.tsx
+++ src/views/dragAndDropList/index.tsx
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useState, type DragEvent } from "react";
 
 type Avenger = {
   id: string;
@@ -31,22 +31,63 @@ const originalAvengers: Avenger[] = [
     title: "Thor",
   },
 ];
-const useAvengerList = <T,>(initialList: T[]) => {
-  const [list, setList] = useState<T[]>(initialList);
-  return { list, setList };
-};
 
 const DragAndDropList = () => {
-  const { list, setList } = useAvengerList<Avenger>(originalAvengers);
+  const [list, setList] = useState<Avenger[]>(originalAvengers);
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+  const [dragged, setDragged] = useState<Avenger|null>(null)
+
+  const onListItemDrag = (e: DragEvent, listItem: Avenger) => {
+    if (!isDragging) {
+      setList((prev) => prev.filter(l => l.id != listItem.id))
+      setIsDragging(true)
+      setDragged(listItem)
+    }
+  };
+
+  const onListItemDrop = (e: DragEvent) => {
+    console.log("On Drop: ", e);
+    setIsDragging(false); 
+    setDragged(null);
+  };
+
+  const onListItemDragEnd = (e: DragEvent) => {
+    console.log("On Drag End: ", e);
+    setIsDragging(false); 
+    setDragged(null);
+  };
+
+  const onListItemDragOver = (e: DragEvent, listItem: Avenger) => {
+    console.log("On Drag Over: ", e);
+  }
 
   return (
-    <div >
-      <div className="text-left text-lg mb-4">Rearrange the original Avengers in order of your favorites.</div>
-      {list.map((listItem: Avenger) => {
-        return <div key={listItem.id} draggable className="my-4 p-2 rounded-md bg-gray-700 w-md">
-          {listItem.title}
-        </div>
-      })}
+    <div>
+      <div className="text-left text-lg mb-4">
+        Rearrange the original Avengers in order of your favorites.
+      </div>
+      <div
+        onDrop={onListItemDrop}
+        className="p-20 bg-gray-950 w-fit"
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {list.map((listItem: Avenger) => {
+          return (
+            <div
+              key={listItem.id}
+              draggable
+              className="my-4 p-2 rounded-md bg-gray-700 w-md"
+              onDrag={(e) => onListItemDrag(e, listItem)}
+              onDragEnd={onListItemDragEnd}
+              onDragOver={(e) => onListItemDragOver(e, listItem)}
+            >
+              {listItem.title}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
