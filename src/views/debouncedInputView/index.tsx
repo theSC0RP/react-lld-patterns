0a0815289dc0850f18d998c21c7683ed4511a794
Gandhar Rajan Bichkar <bichkargandhar@gmail.com>
2025-10-04 05:18:27 -0700

    Input Debouncing: Completed debounced input

--- src/views/debouncedInputView/index.tsx
+++ src/views/debouncedInputView/index.tsx
@@ -1,72 +1,144 @@
-import { useEffect, useState, type ChangeEvent, type InputEvent } from "react";
+import {
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type Dispatch,
+  type ReactElement,
+  type SetStateAction,
+} from "react";
 import Input from "../../components/Input";
 
 type Book = {
-  id: string,
-  authors: string[],
-  title: string
-}
+  id: string;
+  authors: string[];
+  title: string;
+};
+
+const BookList = ({ books }: { books: Book[] }): ReactElement => {
+  return (
+    <div className="mt-4">
+      <div className="text-left text-lg">Books</div>
+      {books.map((book) => {
+        return (
+          <div key={book.id} className="border-1 border-neutral-700 p-2 m-2">
+            <div className="font-bold font-sm">{book?.title}</div>
+            <div className="font-thin">{book?.authors?.join(", ")}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 const DebouncedInputView = () => {
-  const [inputValue, setInputValue] = useState<string>("");
-  const [inputChangeCount, setInputChangeCount] = useState<number>(0);
-  const [inputSearchedBooks, setInputSearchedBooks] = useState<Book[]>([]);
+  const [instantQuery, setInstantQuery] = useState<string>("");
+  const [instantApiCallCount, setInstantApiCallCount] = useState<number>(0);
+  const [instantBookResults, setInstantBookResults] = useState<Book[]>([]);
+
+  const [debouncedQueryInput, setDebouncedQueryInput] = useState<string>("");
+  const [debouncedQuery, setDebouncedQuery] = useState<string>("");
+  const [debouncedApiCallCount, setDebouncedApiCallCount] = useState<number>(0);
+  const [debouncedBookResults, setDebouncedBookResults] = useState<Book[]>([]);
+
   const limit = 10;
 
-  const fetchBooks = async (searchQuery: string) => {
+  const fetchBooks = async (
+    searchQuery: string,
+    stateUpdateFunction: Dispatch<SetStateAction<Book[]>>,
+    countUpdateFunction:Dispatch<SetStateAction<number>>
+  ) => {
     const response = await fetch(
       `https://openlibrary.org/search.json?q=${searchQuery.toLowerCase()}&limit=${limit}`
     );
-    const data = await response.json()
-    const books = data?.docs
-    console.log(books)
-    setInputSearchedBooks(books.map((book:any) => {
-      return {
-        "id": book.cover_i,
-        "authors": book.author_name,
-        "title": book.title
-      }
-    }))
+    const data = await response.json();
+    const books = data?.docs;
+    
+    stateUpdateFunction(
+      books.map((book: any) => {
+        return {
+          id: book.cover_i,
+          authors: book.author_name,
+          title: book.title,
+        };
+      })
+    );
+    countUpdateFunction(prev => prev + 1)
   };
 
   useEffect(() => {
-    if (inputValue) 
-      fetchBooks(inputValue);
-    else
-      setInputSearchedBooks([])
-  }, [inputValue]);
+    if (instantQuery) {
+      fetchBooks(instantQuery, setInstantBookResults, setInstantApiCallCount);
+    } else {
+      setInstantBookResults([]);
+    }
+  }, [instantQuery]);
+
+  useEffect(() => {
+    if (debouncedQueryInput) {
+      const timeoutId = setTimeout(() => setDebouncedQuery(debouncedQueryInput), 500)
+
+      return () => {clearTimeout(timeoutId)}
+    } else {
+      setDebouncedBookResults([])
+    }
+  }, [debouncedQueryInput]);
+
+  useEffect(() => {
+    if (debouncedQuery) {
+      fetchBooks(debouncedQuery, setDebouncedBookResults, setDebouncedApiCallCount);
+    } else {
+      setDebouncedBookResults([])
+    }
+  }, [debouncedQuery])
 
   return (
     <div className="flex w-full gap-2">
       <div className="w-[50%] p-4 border-1 border-neutral-600 rounded-xl">
-        <div className="text-center text-xl font-semibold mb-6">Normal Input</div>
+        <div className="text-center text-xl font-semibold mb-6">
+          Normal Input
+        </div>
         <div className="flex items-center">
-          <label className="mr-2 w-[150px] text-left text-lg">Search Query: </label>
+          <label className="mr-2 w-[150px] text-left text-lg">
+            Search Query:{" "}
+          </label>
           <Input
-            value={inputValue}
+            value={instantQuery}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setInputValue(e.target.value);
-
-              setInputChangeCount((prev) => prev + 1);
+              setInstantQuery(e.target.value);
             }}
             className="rounded-lg p-2 h-[32px] w-full"
-            placeholder="Enter the pokemon you want to search"
+            placeholder="Enter the book you want to search"
           />
         </div>
-        <div className="mt-4">
-          <div className="text-left text-lg">Books</div>
-          {inputSearchedBooks.map(book => {
-            return <div key={book.id} className="border-1 border-neutral-700 p-2 m-2">
-              <div className="font-bold font-sm">{book?.title}</div>
-              <div className="font-thin">{book?.authors?.join(", ")}</div>
-            </div>
-          })}
-        </div>
+        <BookList books={instantBookResults} />
 
-        <div className="mt-4 text-left">API called {inputChangeCount} times</div>
+        <div className="mt-4 text-left">
+          API called {instantApiCallCount} times
+        </div>
       </div>
+
       <div className="w-[50%] p-4 border-1 border-neutral-600 rounded-xl">
-        <div className="text-center text-xl font-semibold mb-6">Debounced Input</div>
+        <div className="text-center text-xl font-semibold mb-6">
+          Debounced Input
+        </div>
+        <div className="flex items-center">
+          <label className="mr-2 w-[150px] text-left text-lg">
+            Search Query:{" "}
+          </label>
+          <Input
+            value={debouncedQueryInput}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setDebouncedQueryInput(e.target.value);
+            }}
+            className="rounded-lg p-2 h-[32px] w-full"
+            placeholder="Enter the book you want to search"
+          />
+        </div>
+        <BookList books={debouncedBookResults} />
+
+        <div className="mt-4 text-left">
+          API called {debouncedApiCallCount} times
+        </div>
       </div>
     </div>
   );
